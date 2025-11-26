7322afade14cded75c4aa1292c3013d4712d6248
Nikita Vasilev <nikita.vasilyev005@gmail.com>
2025-11-26 22:34:25 +0300

    feat: add footer component and integrate it into the main layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,25 @@
 import './App.scss'
 import Header from "./components/layout/header/Header.tsx";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Footer from "./components/layout/footer/Footer.tsx";
 
 function App() {
   return (
     <Router>
-      <Header />
-      <Routes>
-        <Route></Route>
-      </Routes>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+
+        <main className="flex-1">
+          <Routes>
+            <Route></Route>
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
     </Router>
-  )
+  );
 }
+
 
 export default App
